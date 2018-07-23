d035c4383e6bad9ad6fcf1d9d99cac6feb28151e
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-07-23 22:15:07 +0700

    Cập Nhật Mã Nguồn Admin

---
+++ ShopHuy/ShopHuy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopHuy.Startup))]
+namespace ShopHuy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
