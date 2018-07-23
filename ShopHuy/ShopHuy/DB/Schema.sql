dc174a8cdef7892fc0d110f6349f9bcf9750c2bc
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-07-23 22:52:25 +0700

    Cập Nhật Mã Nguồn Admin và User

---
+++ ShopHuy/ShopHuy/DB/Schema.sql
@@ -0,0 +1,4 @@
+﻿Insert into Category([CategoryName],[CategoryDetails]) values(N'Traditionnal',N'Đồng Hồ Truyền Thống');
+Insert into Category([CategoryName],[CategoryDetails]) values(N'Smart',N'Smart');
+Insert into Category([CategoryName],[CategoryDetails]) values(N'Electric',N'Electric');
+
